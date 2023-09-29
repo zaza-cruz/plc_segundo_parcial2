520f9bbd44be5655ee4496b0beb86587f1969050
zaza-cruz <145025887+zaza-cruz@users.noreply.github.com>
2023-09-28 21:32:26 -0600

    Update condicionales01.py

--- condicionales01.py
+++ condicionales01.py
@@ -1,1 +1,8 @@
-
+# Solicitar al usuario que ingrese un número
+numero = float(input("Ingrese un número: "))
+# Comprobar si el número es positivo, negativo o cero if numero > 0:
+print("El número es positivo.")
+elif numero < 0:
+print ("El número es negativo.")
+else:
+print ("El número es igual a cero.")
